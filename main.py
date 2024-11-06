980f74580ebd3f6fde57c6914ce2af438185422c
Pierre Souka <kofipierre2020@gmail.com>
2024-11-06 11:16:19 -0600

    Added 2 basic app routes

---
+++ main.py
@@ -0,0 +1,27 @@
+import os
+
+import requests
+from flask import Flask, render_template, request, redirect, url_for
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+app = Flask(__name__, )
+app.secret_key = "secret"
+
+os.makedirs("static", exist_ok=True)
+
+engine = create_engine('sqlite:///news.db')
+Session = sessionmaker(bind=engine)
+
+conn = "https://api.nytimes.com/svc/archive/v1/2024/11.json?api-key=Uk0DAHWEzvDcpsjumb5ZoGp2GmgtfcSq"
+
+response = requests.get(conn)
+print(response.json())
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
