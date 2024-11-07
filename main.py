67de05859dfdc8e817af89be5d868eb48c1edefb
Pierre Souka <kofipierre2020@gmail.com>
2024-11-07 09:03:15 -0600

    Doing SOmthing

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 import os
-from datetime import datetime
-
 import requests
-from flask import Flask, render_template, request, redirect, url_for, json, session
-from models import*
 from flask import Flask, render_template, request, redirect, url_for
 from models import *
-from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -18,24 +13,17 @@ app.secret_key = "maybe_use_env_file_lol"
 os.makedirs("static", exist_ok=True)
 
 session = Session()
-engine = create_engine('sqlite:///news.db')
-Session = sessionmaker(bind=engine)
 
-@app.route('/', methods=['GET', 'POST'])
-conn = "https://api.nytimes.com/svc/archive/v1/2024/11.json?api-key=Uk0DAHWEzvDcpsjumb5ZoGp2GmgtfcSq"
-
-response = requests.get(conn)
-print(response.json())
-
-@app.route('/')
-def index():
-    if request.method == "POST":
-        session['username'] = request.form['username']
-        session['email'] = request.form['email']
-        session['password'] = request.form['password']  # TODO: Add to Users
-
-        return redirect(url_for('home'))
-    return render_template("login.html")
+# @app.route('/')
+# def index():
+#     if request.method == "POST":
+#         session['username'] = request.form['username']
+#         session['email'] = request.form['email']
+#         session['password'] = request.form['password']  # TODO: Add to Users
+#
+#
+#         return redirect(url_for('home'))
+#     return render_template("login.html")
 
 
 @app.route('/home', methods=['GET', 'POST'])
@@ -44,17 +32,21 @@ def index():
 def home():
     return render_template('home.html')
 
-@app.route('/create_account', methods=['GET', 'POST'])
-def create_account():
-    ss = Session()
+@app.route('/', methods=['GET', 'POST'])
+def index():
+
     if request.method == 'POST':
         name = request.form.get('name')
         email = request.form.get('email')
         password = request.form.get('password')
 
-        new_user = User(username=name, email=email, password=password)
-
-        ss.add(new_user)
+        if validate_user_signup(name, email, password):
+            new_user = User(username=name, email=email, password=password)
+            session.add(new_user)
+            session.commit()
+            return redirect(url_for('home'))
+        else:
+            return render_template('create_account')
 
 
 
