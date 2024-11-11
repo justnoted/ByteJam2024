00cc928e76988f81ee60db807778c4767f6b1675
Pierre Souka <kofipierre2020@gmail.com>
2024-11-11 08:27:56 -0600

    working on the article to display.

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-from flask import Flask
+import json
+from datetime import datetime
+
+import requests
+from flask import Flask, request, redirect, url_for
+
+from models import*
 
 app = Flask(__name__, )
 app.secret_key = "secret"
@@ -17,33 +23,7 @@ app.secret_key = "secret"
 os.makedirs("static", exist_ok=True)
 
 @app.route('/')
-def index():  # put application's code here
-    return render_template("index.html")
-
-@app.route('/home')
-def home():
-    return render_template('home.html')
-
-@app.route('/create_account', methods=['GET', 'POST'])
-def create_account():
-    ss = Session()
-    if request.method == 'POST':
-        name = request.form.get('name')
-        email = request.form.get('email')
-        password = request.form.get('password')
-
-        new_user = User(username=name, email=email, password=password)
-
-        ss.add(new_user)
-@app.route('/home', methods=['GET', 'POST'])
-
-@app.route('/home')
-def home():
-    return render_template('home.html')
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-
+def login():
     if request.method == 'POST':
         name = request.form.get('name')
         email = request.form.get('email')
@@ -55,61 +35,61 @@ def index():
             session.commit()
             return redirect(url_for('home'))
         else:
-            return render_template('create_account')
+            return render_template('login.html')
+    return render_template('welcome.html')
 
-
-
-def get_news_data():
-    conn = "https://api.nytimes.com/svc/archive/v1/2024/11.json?api-key=Uk0DAHWEzvDcpsjumb5ZoGp2GmgtfcSq"
-    response = requests.get(conn)
-    json_response = response.json()
-    # parse_data(json_response)
-    print(json_response)
-
-def parse_data(json_response):
-    articles = []
-    for article in json_response:
-        # pub_date = datetime.strptime(article['pub_date'])
-        parsed_article = {
-            'abstract': article['abstract'],
-            'web_url': article['web_url'],
-            'headline': article['headline'],
-            'pub_date': article['pub_date'],
-            'section_name': article['section_name'],
-            'subsection_name': article['subsection_name'],
-            'keywords': article['keywords']
-        }
-        articles.append(parsed_article)
-        print(parsed_article)
-    return articles
-
-get_news_data()
-
+@app.route("/welcome")
+def welcome():
+    return render_template('welcome.html')
 
 def get_news_data():
-    conn = "https://api.nytimes.com/svc/archive/v1/2024/11.json?api-key=Uk0DAHWEzvDcpsjumb5ZoGp2GmgtfcSq"
-    response = requests.get(conn)
-    json_response = response.json()
-    # parse_data(json_response)
-    print(json_response)
+    try:
+        conn = "https://api.nytimes.com/svc/archive/v1/2024/11.json?api-key=Uk0DAHWEzvDcpsjumb5ZoGp2GmgtfcSq"
+        response = requests.get(conn)
+        json_response = response.json()
+    except Exception as e:
+        print(e)
+
+
+    parse_data(json_response)
+
 
 def parse_data(json_response):
-    articles = []
-    for article in json_response:
-        # pub_date = datetime.strptime(article['pub_date'])
-        parsed_article = {
-            'abstract': article['abstract'],
-            'web_url': article['web_url'],
-            'headline': article['headline'],
-            'pub_date': article['pub_date'],
-            'section_name': article['section_name'],
-            'subsection_name': article['subsection_name'],
-            'keywords': article['keywords']
-        }
-        articles.append(parsed_article)
-        print(parsed_article)
-    return articles
+    try:
+        articles = []
+        docs = json_response["response"]["docs"]
 
+        for article in docs:
+            authors = []
+            if 'byline' in article and 'person' in article['byline']:
+                for person in article['byline']['person']:
+                    author = {
+                        'firstname': person.get('firstname', ''),
+                        'middlename': person.get('middlename', ''),
+                        'lastname': person.get('lastname', ''),
+                        'role': person.get('role', '')
+                    }
+                    authors.append(author)
+
+            # pub_date = datetime.strptime(article['pub_date'], "%Y-%m-%dT%H:%M:%S%z")
+            # formatted_date = pub_date.strftime("%B %d, %Y")
+
+            parsed_article = {
+                'abstract': article.get('abstract', ''),
+                'web_url': article.get('web_url', ''),
+                'headline': article['headline'].get('main', ''),
+                # 'pub_date': formatted_date,
+                'section_name': article.get('section_name', ''),
+                'lead_paragraph': article.get('lead_paragraph', ''),
+                'keywords': [kw.get('value', '') for kw in article.get('keywords', [])],
+                'authors': authors
+            }
+            articles.append(parsed_article)
+
+        return articles
+    except KeyError as e:
+        print(f"Error parsing data: {e}")
+        return None
 get_news_data()
 if __name__ == '__main__':
     app.run()
