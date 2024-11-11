f6d0bd773a3486ba2eafde055557b36c317db5af
note <brayan.cova@outlook.com>
2024-11-11 09:00:32 -0600

    Setup for model tests

--- tests/model_tests.py
+++ tests/model_tests.py
@@ -1,6 +1,35 @@
 import pytest
+from datetime import datetime
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models.models import *
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope='module')
+def test_session():
+    engine = create_engine('sqlite:///:memory:')
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    yield session
+    session.close()
+
+
+def test_validate_user_signup():
+    print("")
+
+
+def test_validate_user_login():
+    print("")
+
+
+def test_check_user_exists():
+    print("")
+
+
+def test_get_user():
+    print("")
+
+
+def test_get_today_decade():
+    datetime.now().year
