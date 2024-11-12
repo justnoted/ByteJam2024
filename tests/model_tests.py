5aa794944966b38ac09ef73d99455ca90508fa1a
Pierre Souka <kofipierre2020@gmail.com>
2024-11-12 09:33:15 -0600

    last updates

--- tests/model_tests.py
+++ tests/model_tests.py
@@ -1,8 +1,6 @@
 import pytest
-from datetime import datetime
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from models.models import *
+
+from back_end.models import *
 
 @pytest.fixture(scope='module')
 def test_session():
@@ -13,23 +11,23 @@ def test_session():
     session = Session()
     yield session
     session.close()
+def test_check_user_exists(test_session):
+    user = User(username="testuser", email="test@example.com", password="TestPassword123!")
+    test_session.add(user)
+    test_session.commit()
 
+    assert check_if_user_exists(test_session, "testuser")
+    assert not check_if_user_exists(test_session, "nonexistentuser")
 
-def test_validate_user_signup():
-    print("")
+def test_get_user(test_session):
+    user = User(username="testuser", email="test@example.com", password="TestPassword123!")
+    test_session.add(user)
+    test_session.commit()
 
-
-def test_validate_user_login():
-    print("")
-
-
-def test_check_user_exists():
-    print("")
-
-
-def test_get_user():
-    print("")
-
+    retrieved_user = get_user_with_username(test_session, "testuser")
+    assert retrieved_user.username == "testuser"
+    assert retrieved_user.email == "test@example.com"
+    assert retrieved_user.password == "TestPassword123!"
 
 def test_get_today_decade():
-    datetime.now().year
+    assert get_today_decade() == datetime.now().year - (datetime.now().year % 10)
